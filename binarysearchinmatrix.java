eb3b9e7854c76f3aaa997ea58e4b958e44e0c116
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-10-16 23:30:29 +0530

    Create binarysearchinmatrix.java
    Create binarysearchinmatrix.java

---
+++ binarysearchinmatrix.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int left = 0;
+        int right = rows * cols - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midElement = matrix[mid / cols][mid % cols];
+            if (midElement == target) {
+                return true;
+            } else if (midElement < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+}
