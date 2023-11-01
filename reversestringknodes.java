58da688d8b48b908cc6740e4af14abeb45069650
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-11-01 19:33:30 +0530

    Create reversestringknodes.java

---
+++ reversestringknodes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String reverseStr(String s, int k) {
+         char[] arr = s.toCharArray();
+        int n = arr.length;
+        for (int i = 0; i < n; i += 2 * k) {
+            int left = i;
+            int right = Math.min(i + k - 1, n - 1);
+            while (left < right) {
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return new String(arr);
+    }
+}
