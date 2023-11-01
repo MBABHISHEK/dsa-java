ee7dc6ce097cf9ac33ce7ac96e11a47065176ab6
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-11-01 19:24:38 +0530

    Create regularexpressionmatching.java

---
+++ regularexpressionmatching.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+
+        if (s == null || p == null) {
+            return false;
+        }
+
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+
+        for (int j = 1; j <= p.length(); j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= p.length(); j++) {
+                if (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2];
+                    if (p.charAt(j - 2) == '.' || p.charAt(j - 2) == s.charAt(i - 1)) {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+
+        return dp[s.length()][p.length()];
+        
+    }
+}
