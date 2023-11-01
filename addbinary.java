6a6a1a2a999826302d0d243258bc750a397e8571
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-11-01 19:44:24 +0530

    Create addbinary.java

---
+++ addbinary.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String addBinary(String a, String b) {
+         StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1, j = b.length() - 1;
+
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a.charAt(i--) - '0';
+            }
+            if (j >= 0) {
+                sum += b.charAt(j--) - '0';
+            }
+            sb.append((char) (sum % 2 + '0'));
+            carry = sum / 2;
+        }
+
+        if (carry != 0) {
+            sb.append((char) (carry + '0'));
+        }
+
+        return sb.reverse().toString();
+    }
+}
