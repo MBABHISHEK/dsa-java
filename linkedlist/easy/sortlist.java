a46e056792127631b509a57431a9ef83cf723894
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-12-12 22:36:42 +0530

    Create sortlist.java

---
+++ linkedlist/easy/sortlist.java
@@ -0,0 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head; // Already sorted if there is 0 or 1 node
+        }
+
+        // Split the list into two halves
+        ListNode middle = findMiddle(head);
+        ListNode secondHalf = middle.next;
+        middle.next = null; // Disconnect the two halves
+
+        // Recursively sort each half
+        ListNode sortedFirstHalf = sortList(head);
+        ListNode sortedSecondHalf = sortList(secondHalf);
+
+        // Merge the sorted halves
+        return merge(sortedFirstHalf, sortedSecondHalf);
+    }
+
+    private ListNode findMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    private ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        // If one of the lists is not empty, append it to the result
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+        return dummy.next;
+    }
+}
