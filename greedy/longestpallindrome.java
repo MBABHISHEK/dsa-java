9f95dfdfe353044eac7ea5e0e2aaaf24b1eb7963
ABHI <mba384420@gmail.com>
2023-12-20 16:40:33 +0530

    new

---
+++ greedy/longestpallindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int oddCount = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : s.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+            if (map.get(ch) % 2 == 1)
+                oddCount++;
+            else
+                oddCount--;
+        }
+        if (oddCount > 1)
+            return s.length() - oddCount + 1;
+        return s.length();
+    }
+}
