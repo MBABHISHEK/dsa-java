021a649816569ef1188badad3b05ca1b3bdc5ea4
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-12-21 21:28:41 +0530

    Create merge.java

---
+++ Matrix/merge.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		
+		for (int i = 0; i < intervals.length; i++) {
+			min = Math.min(min, intervals[i][0]);
+			max = Math.max(max, intervals[i][0]);
+		}
+		
+		int[] range = new int[max - min + 1];
+		for (int i = 0; i < intervals.length; i++) {
+			range[intervals[i][0] - min] = Math.max(intervals[i][1] - min, range[intervals[i][0] - min]); 
+		}
+		
+		int start = 0, end = 0;
+		LinkedList<int[]> result = new LinkedList<>();
+		for (int i = 0; i < range.length; i++) {
+			if (range[i] == 0) {
+				continue;
+			}
+			if (i <= end) {
+				end = Math.max(range[i], end);
+			} else {
+				result.add(new int[] {start + min, end + min});
+				start = i;
+				end = range[i];
+			}
+		}
+		result.add(new int[] {start + min, end + min});
+		return result.toArray(new int[result.size()][]);
+	}
+}
