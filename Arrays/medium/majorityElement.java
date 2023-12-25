e1b95ff8ea9f3362418b5c741920d046b38243c3
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-12-25 07:17:12 +0530

    Create majorityElement.java

---
+++ Arrays/medium/majorityElement.java
@@ -0,0 +1,55 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int count1 = 0, count2 = 0; // Counters for the potential majority elements
+        int candidate1 = 0, candidate2 = 0; // Potential majority element candidates
+
+        // First pass to find potential majority elements.
+        for (int i = 0; i < nums.length; i++) {
+            // If count1 is 0 and the current number is not equal to candidate2, update candidate1.
+            if (count1 == 0 && nums[i] != candidate2) {
+                count1 = 1;
+                candidate1 = nums[i];
+            } 
+            // If count2 is 0 and the current number is not equal to candidate1, update candidate2.
+            else if (count2 == 0 && nums[i] != candidate1) {
+                count2 = 1;
+                candidate2 = nums[i];
+            } 
+            // Update counts for candidate1 and candidate2.
+            else if (candidate1 == nums[i]) {
+                count1++;
+            } else if (candidate2 == nums[i]) {
+                count2++;
+            } 
+            // If the current number is different from both candidates, decrement their counts.
+            else {
+                count1--;
+                count2--;
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int threshold = nums.length / 3; // Threshold for majority element
+
+        // Second pass to count occurrences of the potential majority elements.
+        count1 = 0;
+        count2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (candidate1 == nums[i]) {
+                count1++;
+            } else if (candidate2 == nums[i]) {
+                count2++;
+            }
+        }
+
+        // Check if the counts of potential majority elements are greater than n/3 and add them to the result.
+        if (count1 > threshold) {
+            result.add(candidate1);
+        }
+        if (count2 > threshold) {
+            result.add(candidate2);
+        }
+
+        return result;
+    }
+}
