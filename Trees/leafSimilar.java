ff149bc75737660aa6a25a47354581c38563cccc
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2024-01-09 16:26:51 +0530

    https://leetcode.com/problems/leaf-similar-trees/

---
+++ Trees/leafSimilar.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        List<Integer> leafValues1 = new ArrayList<>();
+        List<Integer> leafValues2 = new ArrayList<>();
+        
+        collectLeafValues(root1, leafValues1);
+        collectLeafValues(root2, leafValues2);
+
+        return leafValues1.equals(leafValues2);
+    }
+
+    private void collectLeafValues(TreeNode root, List<Integer> leafValues) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            leafValues.add(root.val);
+        }
+        collectLeafValues(root.left, leafValues);
+        collectLeafValues(root.right, leafValues);
+    }
+}
