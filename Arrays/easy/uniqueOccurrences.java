a055d0b6b0ec038cee7ccf94b2a8e8f16153c170
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2024-01-17 09:21:44 +0530

    Create uniqueOccurrences.java
    https://leetcode.com/problems/unique-number-of-occurrences/description/

---
+++ Arrays/easy/uniqueOccurrences.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int x : arr) {
+            freq.put(x, freq.getOrDefault(x, 0) + 1);
+        }
+
+        Set<Integer> s = new HashSet<>();
+        for (int x : freq.values()) {
+            s.add(x);
+        }
+
+        return freq.size() == s.size();
+    }
+}
