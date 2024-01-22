7159dd0fb664db0d21addc2ce637af50a582cfd1
ABHI <mba384420@gmail.com>
2024-01-22 09:16:24 +0530

    added 575

---
+++ sets/575distributeCandies.java
@@ -0,0 +1,15 @@
+import java.util.Collections;
+
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        Set<Integer> typeCandySet = new HashSet<>();
+        int halfLength = candyType.length / 2;
+
+        for (int c : candyType) {
+            if (typeCandySet.size() >= halfLength)
+                return halfLength;
+            typeCandySet.add(c);
+        }
+        return Math.min(typeCandySet.size(), halfLength);
+    }
+}
