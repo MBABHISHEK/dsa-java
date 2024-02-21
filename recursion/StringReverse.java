3b4094a9f93356e79f7a02827832cb3296ac410f
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2024-02-21 12:45:15 +0530

    Create StringReverse.java

---
+++ recursion/StringReverse.java
@@ -0,0 +1,13 @@
+class StringReverse
+{
+    /* Function to print reverse of the passed string */
+    void reverse(String str)
+    {
+        if ((str==null)||(str.length() <= 1))
+           System.out.println(str);
+        else
+        {
+            System.out.print(str.charAt(str.length()-1));
+            reverse(str.substring(0,str.length()-1));
+        }
+    }
