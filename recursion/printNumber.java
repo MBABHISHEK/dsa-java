6bf052bc3eff98d9f83517953a4b09e3c0b77708
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2024-02-21 12:20:34 +0530

    Update printNumber.java

--- recursion/printNumber.java
+++ recursion/printNumber.java
@@ -1,5 +1,5 @@
 
-
+/*this prints from 1 TO N*/
 class Solution
 {
   void printNumbers(int N)
@@ -8,5 +8,16 @@ class Solution
     printNumbers(N-1);
     System.out.println(N);
     
+  }
+}
+/*THIS PRINTS FROM N TO 1*/
+
+class Solution
+{
+   void printNumbers(int N)
+  {
+    System.out.println(N);
+    if(N==0)return ;
+    printNumbers(N-1);
   }
 }
